56c44ba515de0ffd4764b1e7da51c8c9d8a65d7a
Vinicius Lima <viniciuslima957@hotmail.com>
2025-10-24 15:21:50 -0300

    feat: Criando verificador da força da senha

--- frontend/src/app/pages/cadastro/cadastro.component.ts
+++ frontend/src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, OnInit, signal} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
@@ -10,6 +10,7 @@ import { FormBannerLayoutComponent } from "../../components/form-banner-layout/f
 import { compararSenhaValidator } from '../../validators/compararSenha.validator';
 import { AuthService } from '../../services/auth/auth.service';
 import { cadastrarColaboradorDTO, cadastrarUsuarioDTO } from '../../types/types';
+import { NgClass } from "@angular/common";
 
 
 @Component({
@@ -22,30 +23,39 @@ import { cadastrarColaboradorDTO, cadastrarUsuarioDTO } from '../../types/types'
     RouterLink,
     ReactiveFormsModule,
     FormBannerLayoutComponent,
-    MatSelectModule
-  ],
+    MatSelectModule,
+    NgClass
+],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
   mostrarSenha = signal(true);
   cadastroForm!: FormGroup;
+  forcaDaSenha = signal<string>("");
+  private passwordRegex: string = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*\\-_])[A-Za-z\\d!@#$%^&*\\-_]{8,}$';
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService
-  ){}
+  ){
+    
+  }
 
   ngOnInit(): void {
     this.cadastroForm = this.fb.group({
       nome: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/^[\p{L}\s'-]+$/u)]],
       email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.minLength(3)]],
+      senha: ['', [Validators.required, Validators.minLength(3), Validators.pattern(this.passwordRegex)]],
       confirmarSenha: ['', [Validators.required]]
     }, { validators:compararSenhaValidator('senha','confirmarSenha')});
-  }
-  
 
+    this.cadastroForm.get('senha')?.valueChanges.subscribe((senha)=>{
+      this.verificarForcaDaSenha(senha)
+    })
+  }
+
+  
   enviar(){
     const cadastrarUsuarioDTO:cadastrarUsuarioDTO = {
       Username: this.cadastroForm.value.nome,
@@ -63,6 +73,39 @@ export class CadastroComponent {
       },
     });
 
+  }
+
+  verificarForcaDaSenha(senha:string){
+    const especiais = /[!@#$%^&*(),.?":{}|<>_\-+=~[\]\\;'/`]/;
+    const letrasMaiusculasEMinusculas = /[A-Z]/;
+    const apenasMinusculas = /[a-z]/;
+    const numeros = /[0-9]/;
+
+    if( letrasMaiusculasEMinusculas.test(senha) && 
+        apenasMinusculas.test(senha) &&
+        numeros.test(senha) &&
+        especiais.test(senha) && 
+        senha.length>=3
+    ){
+      this.forcaDaSenha.set('forte');
+      return;
+    }
+    
+    if(letrasMaiusculasEMinusculas.test(senha) && 
+        apenasMinusculas.test(senha) &&
+        senha.length>=3){
+      this.forcaDaSenha.set('media');
+      return;
+    }
+    
+    if(apenasMinusculas.test(senha) &&
+        senha.length>=3){
+      this.forcaDaSenha.set('fraca');
+      return;
+    }
+    
+    this.forcaDaSenha.set('');
+    
   }
 
   esconder(){
