508b874088a0347da9dd26990434e3f4c6df1800
Vinicius Lima <viniciuslima957@hotmail.com>
2025-10-30 16:52:55 -0300

    feat: mudando o titulo das paginas de acordo com as rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,11 +9,13 @@ export const routes: Routes = [
     },
     {
         path:'login',
-        loadComponent: ()=> import('./pages/login/login.component').then((c)=>(c.LoginComponent))
+        loadComponent: ()=> import('./pages/login/login.component').then((c)=>(c.LoginComponent)),
+        title:"Login | Ponto fácil"
     },
     {
         path:'cadastro',
-        loadComponent: ()=> import('./pages/cadastro/cadastro.component').then((c)=>(c.CadastroComponent))
+        loadComponent: ()=> import('./pages/cadastro/cadastro.component').then((c)=>(c.CadastroComponent)),
+        title:"Cadastro | Ponto fácil"
     },
     {
         path:'portal',
@@ -28,27 +30,32 @@ export const routes: Routes = [
             {
                 path:'gerencia/colaboradores',
                 loadComponent: ()=> import('./pages/gerenciar-colaboradores/gerenciar-colaboradores.component').then((c)=>(c.GerenciarColaboradoresComponent)),
-                canActivate: [authGuard]
+                canActivate: [authGuard],
+                title:"Colaboradores | Ponto fácil"
             },
             {
                 path:'gerencia/setores',
                 loadComponent: ()=> import('./pages/gerenciar-setores/gerenciar-setores.component').then((c)=>(c.GerenciarSetoresComponent)),
-                canActivate: [authGuard]
+                canActivate: [authGuard],
+                title:"Setores | Ponto fácil"
             },
             {
                 path:'gerencia/cargos',
                 loadComponent: ()=> import('./pages/gerenciar-cargos/gerenciar-cargos.component').then((c)=>(c.GerenciarCargosComponent)),
-                canActivate: [authGuard]
+                canActivate: [authGuard],
+                title:"Cargos | Ponto fácil"
             },
             {
                 path:'perfil',
                 loadComponent: ()=> import('./pages/perfil/perfil.component').then((c)=>(c.PerfilComponent)),
-                canActivate: [authGuard]
+                canActivate: [authGuard],
+                title:"Meu perfil | Ponto fácil"
             },
             {
                 path:'dashboard',
                 loadComponent: ()=> import('./pages/dashboard/dashboard.component').then((c)=>(c.DashboardComponent)),
-                canActivate: [authGuard]
+                canActivate: [authGuard],
+                title:"Dashboard | Ponto fácil"
             }
         ]
     },
