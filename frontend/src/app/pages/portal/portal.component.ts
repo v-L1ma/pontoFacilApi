837eeb0d4cae3fb859a6f07243eabd83bb48a6f9
Vinicius Lima <viniciuslima957@hotmail.com>
2025-10-31 17:02:36 -0300

    feat: criando uma side bar

--- frontend/src/app/pages/portal/portal.component.ts
+++ frontend/src/app/pages/portal/portal.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
-import { HeaderComponent } from '../../shared/components/header/header.component'; 
+import { SideBarComponent } from '../../shared/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-portal',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.scss'
 })
